Evolution-X-AOSP/packages_apps_Launcher3

Launcher3: Add toggle to show/hide app drawer search bar
Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
Signed-off-by: minaripenguin <alexfinhart@gmail.com>
Signed-off-by: Rizky Benggolo <travarilo@gmail.com>

src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
@@ -168,6 +168,12 @@ public class ActivityAllAppsContainerView<T extends Context & AppLauncher
           getSearchView().setBackgroundResource(R.drawable.bg_all_apps_searchbox_google_themed);
         }
         
+        if (Utilities.showSearchBar(getContext())) {
+            mSearchContainer.setVisibility(View.VISIBLE);
+        } else {
+            mSearchContainer.setVisibility(View.GONE);
+        }
+
         mSearchUiManager = (SearchUiManager) mSearchContainer;
         mSearchUiManager.initializeSearch(this);
     }

src/com/android/launcher3/settings/SettingsAppDrawer.java
@@ -96,7 +96,15 @@ public class SettingsAppDrawer extends CollapsingToolbarBaseActivity
     }
 
     @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) { }
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        switch (key) {
+            case Utilities.KEY_DRAWER_SEARCHBAR:
+                LauncherAppState.getInstanceNoCreate().setNeedsRestart();
+                break;
+            default:
+                break;
+        }
+    }
 
     private boolean startPreference(String fragment, Bundle args, String key) {
         if (Utilities.ATLEAST_P && getSupportFragmentManager().isStateSaved()) {

Rafiester/packages_apps_ThemePicker

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
@@ -70,6 +70,11 @@ public class ActivityAllAppsContainerView<T extends Context & ActivityContext>
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
+        if (Utilities.showSearch(getContext())) {
+            mSearchContainer.setVisibility(View.VISIBLE);
+        } else {
+            mSearchContainer.setVisibility(View.GONE);
+        }
         mSearchUiManager.initializeSearch(this);
     }
 

src/com/android/launcher3/settings/SettingsAppDrawer.java
@@ -98,7 +98,15 @@ public class SettingsAppDrawer extends CollapsingToolbarBaseActivity
     }
 
     @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) { }
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        switch (key) {
+            case Utilities.KEY_DRAWER_SEARCH:
+                LauncherAppState.getInstanceNoCreate().setNeedsRestart();
+                break;
+            default:
+                break;
+        }
+    }
 
     private boolean startPreference(String fragment, Bundle args, String key) {
         if (Utilities.ATLEAST_P && getSupportFragmentManager().isStateSaved()) {